3bfd37636214b9c3a7dc88f49dd65b1cc1b85528
ramekabal <ramekbal159@gmail.com>
2024-11-09 19:22:59 +0530

    setup and check serv is running or not

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("Server runninsg...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.get("/contact", (req, res) => {
+  res.send("Runnning...");
+});
+
+app.listen(port, () => {
+  console.log("Server runnig...", port);
+});
