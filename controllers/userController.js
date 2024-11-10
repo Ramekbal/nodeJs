27516ce2d3c1fb80fe9c2aa3df88744eb854650d
ramekabal <ramekbal159@gmail.com>
2024-11-10 09:00:42 +0530

    login and register

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModal");
 
 //@Desc Register a new user
@@ -34,7 +35,37 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-  res.status(200).json("Login...");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).json("Please add all fields");
+    throw new Error("Please add all fields");
+  }
+  const emailExits = await User.findOne({ email });
+  if (!emailExits) {
+    res.status(400).json("Please Register first");
+    throw new Error("Please Register first");
+  }
+  const isPasswordMatch = await bcrypt.compare(password, emailExits.password);
+  if (isPasswordMatch) {
+    const token = jwt.sign(
+      {
+        user: {
+          userName: emailExits.userName,
+          email: emailExits.email,
+          id: emailExits._id,
+        },
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    console.log("calling");
+    res.status(200).json({ token });
+  } else {
+    res.status(401).json("Invalid Password");
+    throw new Error("Invalid Password");
+  }
+
+  //   res.status(200).json("Login...");
 });
 
 const userInfo = asyncHandler(async (req, res) => {
